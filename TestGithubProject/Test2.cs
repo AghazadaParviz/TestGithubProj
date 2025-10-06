2c90831f0760ed77c1aa41c42d88b9a0dbc396ea
Parviz <parvizagazade96@gmail.com>
2025-10-06 14:51:38 +0400

    1st commit from test2
    2nd commit from test2
    
    3rd commit from test2
    
    4th commit from test2

---
+++ TestGithubProject/Test2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGithubProject
+{
+    public class Test2
+    {
+        public Test2()
+        {
+            Console.WriteLine("1st commit from test2");
+            Console.WriteLine("2nd commit from test2");
+            Console.WriteLine("3rd commit from test2");
+            Console.WriteLine("4th commit from test2");
+        }
+    }
+}
