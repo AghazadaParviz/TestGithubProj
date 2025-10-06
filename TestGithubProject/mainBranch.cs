1f05cde52a6f23acc57a2bfa979c56b5c207c2c4
Parviz <parvizagazade96@gmail.com>
2025-10-07 02:21:22 +0400

    3rd commit from main

---
+++ TestGithubProject/mainBranch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGithubProject
+{
+    internal class mainBranch
+    {
+        public mainBranch()
+        {
+            Console.WriteLine("Main branch 3rd commit");
+        }
+    }
+}
