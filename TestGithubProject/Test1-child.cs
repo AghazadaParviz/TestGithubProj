4d5a579e9f70a6d8a604f572155fc7eb6d8514ad
Parviz <parvizagazade96@gmail.com>
2025-10-06 11:33:01 +0400

    1st commit from test1-child

---
+++ TestGithubProject/Test1-child.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGithubProject
+{
+    public class Test1_child
+    {
+        public Test1_child()
+        {
+            Console.WriteLine("1st commit from test1-child");
+        }
+    }
+}
