c362db2ba5fb2ab429c8f91dcf13d301d0b7e8bc
Parviz <parvizagazade96@gmail.com>
2025-10-06 11:35:13 +0400

    2nd commit from test1

--- TestGithubProject/Program.cs
+++ TestGithubProject/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("1st commit from test1");
+            Console.WriteLine("2nd commit from test1");
         }
     }
 }
