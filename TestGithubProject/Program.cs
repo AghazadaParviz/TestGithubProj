59b062c8aa77cf8901359bd950f033d90987603d
Parviz <parvizagazade96@gmail.com>
2025-10-10 23:50:32 +0400

    4th commit from test1

--- TestGithubProject/Program.cs
+++ TestGithubProject/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("1st commit from test1");
             Console.WriteLine("2nd commit from test1");
             Console.WriteLine("3rd commit from test1");
+            Console.WriteLine("4th commit from test1");
         }
     }
 }
